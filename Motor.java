4ab4a58ce589406f780d5e0ae8185b5a57c2d92a
seanPruss <seanpruss5@gmail.com>
2024-01-29 09:32:26 -0500

    motor class + separated door open and close methods for readability

--- Motor.java
+++ Motor.java
@@ -3,4 +3,17 @@
  */
 public class Motor {
 
+    private Boolean isRunning = false;
+
+    public Boolean isMotorRunning() {
+        return isRunning;
+    }
+
+    public void start() {
+        isRunning = true;
+    }
+
+    public void stop() {
+        isRunning = false;
+    }
 }
