889aed9bb39811f6eefac487f25eba542408c577
seanPruss <seanpruss5@gmail.com>
2024-01-29 08:56:48 -0500

    button class

--- ElevatorButton.java
+++ ElevatorButton.java
@@ -2,5 +2,14 @@
  * ElevatorButton
  */
 public class ElevatorButton {
+    private Integer floor;
+
+    public ElevatorButton(Integer floor) {
+        this.floor = floor;
+    }
+
+    public Integer press() {
+        return floor;
+    }
 
 }
