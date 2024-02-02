fa0834a041903bb2ea78a3c83a8de0f8652a7a95
kavvykav <liamkavanagh81@gmail.com>
2024-02-02 17:47:22 -0500

    Updated Main.java to start the Floor subsystem

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Thread scheduler, elevator;
+        Thread scheduler, elevator, floor;
         int numFloors = 22;
 
+        floor = new Thread(new Floor("test_input.txt"), "Floor");
         scheduler = new Thread(new Scheduler(), "Scheduler");
         elevator = new Thread(new Elevator(numFloors), "Elevator");
 
+        floor.start();
         scheduler.start();
         elevator.start();
     }
