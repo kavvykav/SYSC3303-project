6b041ce2df77dac4965680cc8cf9518125f92e18
mhuybregts <123901369+mhuybregts@users.noreply.github.com>
2024-02-08 20:10:37 -0500

    refactor: Implemented inheritance for Floor and Elevator class
    Resolves: #2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Main {
+
+    private static final int SERVER_PORT = 5000;
+    private static final String INPUT_FILE = "src/test_input.txt";
+
     public static void main(String[] args) {
+
         Thread scheduler, elevator, floor;
         int numFloors = 22;
+        InetAddress localHost;
+
+        try {
+            localHost = InetAddress.getLocalHost();
+        } catch(UnknownHostException e) {
+            return;
+        }
 
         scheduler = new Thread(new Scheduler(), "Scheduler");
-        elevator = new Thread(new Elevator(numFloors), "Elevator");
-        floor = new Thread(new Floor("test_input.txt"), "Floor");
+        elevator = new Thread(new Elevator(numFloors, localHost, SERVER_PORT), "Elevator");
+        floor = new Thread(new Floor(INPUT_FILE, localHost, SERVER_PORT), "Floor");
 
         scheduler.start();
         elevator.start();
