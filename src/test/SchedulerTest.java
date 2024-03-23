e536977b0c0f6e7153531553d8eddae16f2bac50
mhuybregts <123901369+mhuybregts@users.noreply.github.com>
2024-03-22 22:49:49 -0400

    fix: Update Scheduler algorithm
    Account for when an elevator is currently on the same floor as a request

--- src/test/SchedulerTest.java
+++ src/test/SchedulerTest.java
@@ -83,10 +83,10 @@ class SchedulerTest {
         assertEquals(elevator4, scheduler.chooseElevator(data2));
 
         FloorData data3 = new FloorData("2:35:25", 1, true, 8);
-        assertEquals(elevator1, scheduler.chooseElevator(data3));
+        assertEquals(elevator3, scheduler.chooseElevator(data3));
 
         FloorData data4 = new FloorData("3:35:25", 20, true, 22);
-        assertEquals(elevator3, scheduler.chooseElevator(data4));
+        assertEquals(elevator1, scheduler.chooseElevator(data4));
     }
     @Test
     void CanServiceRequestTest(){
