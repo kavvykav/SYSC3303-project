dfb9b00722d0582b92c1795f1c29c15c4e14c093
kavvykav <liamkavanagh81@gmail.com>
2024-04-10 11:11:25 -0400

    refactor: fixed AcceptanceTest.java

--- src/floor/AcceptanceTest.java
+++ src/floor/AcceptanceTest.java
@@ -8,63 +8,75 @@ import scheduler.Scheduler;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import common.NetworkConstants;
 import static common.NetworkConstants.GUI_PORT;
 import static common.NetworkConstants.SCHEDULER_PORT;
 
-public class AcceptanceTest {
-    private static final int SERVER_PORT = 5000;
+public class AcceptanceTest implements Runnable{
+
+    private int threadNumber;
     private static final String INPUT_FILE = "test_input.txt";
+
+    private static Scheduler scheduler;
+    private static GUI gui;
+    private static Floor floor;
+
+    public AcceptanceTest(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+    /**
+     * Thread routine to start the subsystems.
+     */
+    public void run() {
+        if (threadNumber == 1) {
+            scheduler = new Scheduler();
+            scheduler.serveRequests();
+        } else if (threadNumber == 2) {
+            Console console = new Console();
+            gui = new GUI(NetworkConstants.localHost(), GUI_PORT, console);
+            gui.receiveUpdates();
+        } else if (threadNumber == 3) {
+            floor = new Floor(INPUT_FILE, NetworkConstants.localHost(), NetworkConstants.SCHEDULER_PORT);
+            Thread sender = new Thread(new Sender(floor), "Sender");
+            Thread receiver = new Thread(new Receiver(floor), "Receiver");
+            sender.start();
+            receiver.start();
+        }
+    }
+
+    /**
+     * Wrapper method for sleep. Sleeps for 500 ms.
+     */
+    public static void sleep() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 
     public static void main(String[] args) {
 
         int numFloors = 22;
-        InetAddress localHost;
 
-        try {
-            localHost = InetAddress.getLocalHost();
-        } catch (UnknownHostException e) {
-            return;
-        }
+        Thread schedulerThread = new Thread(new AcceptanceTest(1), "Scheduler");
+        schedulerThread.start();
+        sleep();
 
-        Console console = new Console();
-        Floor floor = new Floor(INPUT_FILE, localHost, SERVER_PORT);
-        Scheduler scheduler = new Scheduler();
-        GUI guiClient = new GUI(localHost, GUI_PORT, console);
-
-        scheduler.serveRequests();
-
-        try{
-            Thread.sleep(1000);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
-
-        guiClient.receiveUpdates();
-        try{
-            Thread.sleep(1000);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
+        Thread gui = new Thread(new AcceptanceTest(2), "GUI");
+        gui.start();
+        sleep();
 
         for (int i=1; i<4+1; i++) {
-            Elevator elevator = new Elevator(numFloors, localHost, SCHEDULER_PORT, i);
+            Elevator elevator = new Elevator(numFloors, NetworkConstants.localHost(), SCHEDULER_PORT, i);
             Thread thread = new Thread(elevator, "Elevator " + i);
             thread.start();
         }
-        try{
-            Thread.sleep(1000);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
+        sleep();
 
-        floor.sendRequests();
-        floor.receiveResponses();
-        try{
-            Thread.sleep(1000);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
-
-        System.out.println("Efficiency of the system: " + scheduler.getStats().getNumServed() / scheduler.getStats().getNumRequests());
+        Thread floor = new Thread(new AcceptanceTest(3), "Floor");
+        floor.start();
     }
 }
